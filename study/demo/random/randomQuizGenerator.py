a18340cd15a2fe38d86eb3ffd75f40e01299fe60
SunnySun <240746804@qq.com>
2023-03-17 22:29:35 +0800

    2023-03-17-02

--- study/demo/random/randomQuizGenerator.py
+++ study/demo/random/randomQuizGenerator.py
@@ -1,47 +1,33 @@
 import random, requests
 from pathlib import Path
+import numpy as np
 
 # 从网络请求数据
 # url = 'http://apis.juhe.cn/fapigw/globalarea/areas?key=15de6af29786008b8a0b8eeaf3b23279&country=%E4%B8%AD%E5%9B%BD'
 # area_list = requests.get(url).content.decode('utf-8')
 # fileObj = open(Path('area_list.txt'), 'w')
-# fileObj.write('area_list = ' + area_list + '\n')
+# fileObj.write(area_list + '\n')
 
 # 从本地读取
 area_content = open(Path('area_list.txt'), 'r').readlines()
-area_list = list(area_content)
+# 数据类型转换
+area_str = str(area_content)
 
-print('area_list', area_list)
-print('area_list_len', len(area_list))
+# 数据切片
+sub_start_index = area_str.find('[{')
+sub_end_index = area_str.find('}]')
+data_content = area_str[sub_start_index + 1: sub_end_index+1]
 
-for area in area_list:
-    print('area', area)
+if data_content.endswith('}'):
+    c = data_content.split('},')
+    print("c == ", c)
+    newArr = []
+    for i in c:
+        newItem = i + '}'
+        newArr.append(newItem)
+        print("newItem == ", newItem)
 
-# 遍历城市
-# for area in range(area_list):
-#     print('area = ', area)
-
-# print("area_list", area_list)
-
-# for area in area_list:
-#     # res = str(area).encode('utf-8')
-#     print("area = ", area.result)
-
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
-            'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
-            'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
-            'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
-                'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
-                'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
-                'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
-                'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
-                'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
-                'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'NewMexico': 'Santa Fe',
-            'New York': 'Albany', 'North Carolina': 'Raleigh',
-            'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-            'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
-            'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
-                'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-                'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'WestVirginia': 'Charleston',
-            'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
-# Generate
+    # print('newArr', newArr)
+    print('newArr_length', len(newArr))
+# print('content==', data_content)
+# print('area_arr==', area_arr)
