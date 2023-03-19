5c8aaca37c45e778d2b9a468648caa9ba8d3e3cf
SunnySun <240746804@qq.com>
2023-03-19 20:49:57 +0800

    2023-03-19

--- study/demo/random/randomQuizGenerator.py
+++ study/demo/random/randomQuizGenerator.py
@@ -9,17 +9,54 @@ import numpy as np
 # fileObj.write(area_list + '\n')
 
 # 从本地读取
-capitals = {'湖南省': '长沙市', '广东省': '广州市', '江西省': '南昌市',
-            '湖北省': '武汉市', '河南省': '郑州市', '河北省': '石家庄市'}
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
+            'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver',
+            'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
+            'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois':
+                'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas':
+                'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine':
+                'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
+                'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
+                'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
+                'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'NewMexico': 'Santa Fe',
+            'New York': 'Albany', 'North Carolina': 'Raleigh',
+            'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+            'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+            'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': 'Austin',
+            'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia',
+            'WestVirginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
 for quizNum in range(35):
     # 定义测试和答案文件
-    quizFile = open(f'题目{quizNum + 1}.txt', 'w')
-    answerKeyFile = open(f'答案{quizNum + 1}.txt', 'w')
+    quizFile = open(f'capitalsquiz{quizNum + 1}.txt', 'w+')
+    answerKeyFile = open(f'capitalsquiz_answers{quizNum + 1}.txt', 'w+')
     # 定义测试卷头部信息
-    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write('Name:\n\n Date:\n\nPeriod:\n\n')
     quizFile.write((' ' * 20) + f'State Capitals Quiz (Form{quizNum + 1})')
     quizFile.write('\n\n')
     # Shuffle the order of the states.
     states = list(capitals.keys())
     random.shuffle(states)
+
+    for questionNum in range(50):
+        # 获取正确和错误答案
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+
+        # 写入答卷及题目
+        quizFile.write(f'{questionNum + 1}. What is the capital of {states[questionNum]}?\n')
+        for i in range(4):
+            quizFile.write(f'{"ABCD"[i]} . {answerOptions[i]}\n')
+            quizFile.write(f'\n')
+            # 写入答案到文件
+            answerKeyFile.write(f"{questionNum + 1}.{'ABCD'[answerOptions.index(correctAnswer)]}")
+            # 这个关闭文件，打开会出现，文件不全，应该要先判断是否完整，再关闭
+            # 此处待完善
+            # quizFile.close()
+            # answerKeyFile.close()
+
+
