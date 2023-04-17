3e9faf4f505b3b088909d5aa886982242ea53b04
admin <240746804@qq.com>
2023-04-17 16:49:13 +0800

    04017-3

---
+++ study/demo/PyPDF2/merge_all_pdf.py
@@ -0,0 +1,28 @@
+import pathlib
+
+import PyPDF2, os
+
+pdfFiles = []
+pdf_files_path = r'C:\Users\admin\PycharmProjects\pythonProject3\study\pdf'
+
+for filename in os.listdir(pdf_files_path):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+
+pdfFiles.sort(key=str.lower)
+pdfWriter = PyPDF2.PdfWriter()
+print('pdf files', pdfFiles)
+
+# 遍历所有PDF文件
+for filename in pdfFiles:
+    pdf_file_Obj = open(pdf_files_path + '\\' + f'{filename}', 'rb')
+    pdf_file_reader = PyPDF2.PdfReader(pdf_file_Obj)
+    # 遍历所有页面，除了第一页
+    for pageNum in range(1, len(pdf_file_reader.pages)):
+        pageObj = pdf_file_reader.pages[pageNum]
+        pdfWriter.add_page(pageObj)
+
+# 保存结果页到文件
+pdfOutput = open('all_merge_page.pdf', 'wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
