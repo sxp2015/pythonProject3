28eb7690e9bcf7eb4739f37a3ee388cca82705f1
SunnySun <240746804@qq.com>
2023-04-28 21:35:10 +0800

    2023-04-28-1

--- study/demo/spider/requests/test_get.py
+++ study/demo/spider/requests/test_get.py
@@ -1,15 +1,41 @@
 import requests
 import re
+from faker import Faker
+# 生成随机请求头
+from fake_user_agent import user_agent
 
+# 创建实例
+ua = Faker()
+
+# 定义请求头
+headers = {
+    'User-Agent': ua.user_agent()
+}
+
+headers2 = {
+    'User-Agent': user_agent()
+}
+
+# 定义数据
 data = {
     'name': 'germey',
     'age': 15
 }
 
-r = requests.get('https://www.baidu.com')
+# 定义网站
+r = requests.get('https://www.baidu.com/')
 r2 = requests.get('https://www.httpbin.org/get', params=data)
-
 r3 = requests.get('https://dog.ceo/')
+r4 = requests.get('https://p5.lw05.cn/favicon.ico')
+r5 = requests.get('https://p5.lw05.cn/', headers=headers)
+
+print('r5 headers', r5.text)
+
+
+# 写入图片
+with open('favicon.ico', 'wb') as f:
+    f.write(r4.content)
+
 pattern = re.compile('<h2.*?>(.*?)</h2>', re.S)
 titles = re.findall(pattern, r3.text)
 
@@ -19,7 +45,6 @@ print('status_code:', r.status_code)
 print('r.text:', type(r.text))
 print('r.text[:100]:', r.text[:100])
 print('r.cookies:', r.cookies)
-print('===================================')
 
 print('r2:', r2.text)
 print('r2.url:', r2.url)
