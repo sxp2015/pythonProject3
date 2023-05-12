93971784f22db82e2ab0a0b439cbc514bc4e6a80
admin <240746804@qq.com>
2023-05-12 17:39:18 +0800

    230512-2

--- study/demo/BaiJiaHao/copyBaijiaToWechat.py
+++ study/demo/BaiJiaHao/copyBaijiaToWechat.py
@@ -25,7 +25,7 @@ browser.get(BaiJiaHao_URL)
 # 获取当前窗口句柄
 current_window_handle = browser.current_window_handle
 # 执行 JavaScript 语句，在新窗口中打开指定的网页
-browser.execute_script("window.open('https://mp.weixin.qq.com/')")
+# browser.execute_script("window.open('https://mp.weixin.qq.com/')")
 # 切换到新窗口
 browser.switch_to.window(browser.window_handles[0])
 # 等待5秒切换界面
@@ -35,7 +35,7 @@ input_userName = browser.find_element("id", "TANGRAM__PSP_4__userName")
 input_password = browser.find_element("id", "TANGRAM__PSP_4__password")
 button_submit = browser.find_element("id", "TANGRAM__PSP_4__submit")
 input_userName.send_keys("风车百科")
-input_password.send_keys("s..............0")
+input_password.send_keys("s。。。。。。。。。410")
 button_submit.click()
 
 # 等待滑块验证完成切换界面
@@ -74,30 +74,29 @@ if image_location:
     # pyautogui.click(image_location)
     # 等待内容数据显示出来
     time.sleep(6)
-    # 移动到更多
-    pyautogui.moveTo(1234, 450, 1)
-    # 等待下拉列表显示出来
-    time.sleep(2)
-    # 使用selenium获取更多列表的选项内容
-    dropdown = browser.find_element(By.CLASS_NAME, 'cheetah-popover-inner-content')
-    # 如果没有增加商品这个选项的样式，说明是文章内容
-    # 输出文本
-    print('选项文字内容：', dropdown.text)
+
+
+    # 定义查找标签元素的方法
+    def get_element_location(xpath: str):
+        elements = browser.find_elements(By.XPATH, xpath)
+        if not elements:
+            return None
+        # 获取窗口当前的滚动距离
+        location = elements[0].location
+        screen_x = location['x']
+        screen_y = location['y']
+        print('location: ==', location)
+        print('screen_x:', screen_x, 'screen_y:', screen_y)
+        return screen_x, screen_y
+
 
     while True:
-        # 定义查找标签元素的方法
-        def get_element_location(xpath: str):
-            elements = browser.find_elements(By.XPATH, xpath)
-            if not elements:
-                return None
-            # 获取窗口当前的滚动距离
-            location = elements[0].location_once_scrolled_into_view
-            screen_x = location['x']
-            screen_y = location['y']
-            print('location: ==', location)
-            print('screen_x:', screen_x, 'screen_y:', screen_y)
-            return screen_x, screen_y
-
+        # 移动到更多
+        pyautogui.moveTo(1234, 450, 1)
+        # 等待下拉列表显示出来
+        time.sleep(2)
+        # 使用selenium获取更多列表的选项内容
+        dropdown = browser.find_element(By.CLASS_NAME, 'cheetah-popover-inner-content')
 
         # 定义Xpath规则
         xpath_revoke = '//span[contains(text(), "撤回")]'
@@ -105,59 +104,58 @@ if image_location:
         # 获取下拉文本列表
         dropdown_text = dropdown.text
 
-        # 创建 ActionChains 对象
-        actions = ActionChains(browser)
-
         # 判断列表的内容
         if '删除' in dropdown_text:
             remove_location = get_element_location(xpath_delete)
             # 识别删除选项所在的位置
             locate_shan_chu = pyautogui.locateOnScreen(SHAN_CHU)
-
             if remove_location is not None:
-
-                remove_x, remove_y = remove_location
-
-                # print('remove_x:', remove_x, 'remove_y:', remove_y)
-                # result = actions.move_by_offset(remove_x, remove_y) .click().perform()
                 if locate_shan_chu:
                     print('删除位置识别成功', locate_shan_chu)
-                    # shan_chu_x, shan_chu_y = pyautogui.center(locate_shan_chu)
-                    # print('shan_chu_x:', shan_chu_x, 'shan_chu_y:', shan_chu_y)
-                    # pyautogui.moveTo(shan_chu_x, shan_chu_y, duration=0.25)
-                    pyautogui.click(locate_shan_chu)
-                    time.sleep(10)
-
-                # 点击 remove_location 位置
-                # print('result = ', result)
-
+                    pyautogui.moveTo(locate_shan_chu.left + 25, locate_shan_chu.top + 5, duration=1)
+                    pyautogui.click()
+                    time.sleep(1)
+                    pyautogui.moveTo(locate_shan_chu.left - 480, locate_shan_chu.top - 80, duration=1)
+                    pyautogui.click()
+                    time.sleep(5)
+                    pyautogui.moveTo(1234, 450, 1)
+                    time.sleep(2)
+                    # 使用selenium获取更多列表的选项内容
+                    dropdown = browser.find_element(By.CLASS_NAME, 'cheetah-popover-inner-content')
+                    if not dropdown_text:
+                        break
+                    else:
+                        continue
             else:
                 print('位置获取失败')
-                time.sleep(10)
+                time.sleep(5)
 
-        if '撤回' in dropdown_text:
+        elif '撤回' in dropdown_text:
             return_location = get_element_location(xpath_revoke)
             # 识别撤回选项所在的位置
             locate_che_hui = pyautogui.locateOnScreen(CHE_HUI)
             if return_location is not None:
-                return_x, return_y = return_location
-                # print('return_x:', return_x, 'return_y:', return_y)
-                # result = actions.move_by_offset(return_x, return_y).click().perform()
                 if locate_che_hui:
                     print('撤回位置识别成功', locate_che_hui)
-                    # che_hui_x, che_hui_y = locate_che_hui.left, locate_che_hui.top
-                    # # print('che_hui_x:', che_hui_x, 'che_hui_y:', che_hui_y)
-                    # pyautogui.moveTo(che_hui_x, che_hui_y, duration=0.25)
                     pyautogui.click(locate_che_hui)
-                    time.sleep(10)
-                # print('result = ', result)
+                    time.sleep(1)
+                    pyautogui.moveTo(locate_che_hui.left - 480, locate_che_hui.top - 140, duration=1)
+                    pyautogui.click()
+                    time.sleep(5)
+                    pyautogui.moveTo(1234, 450, 1)
+                    time.sleep(2)
+                    # 使用selenium获取更多列表的选项内容
+                    dropdown = browser.find_element(By.CLASS_NAME, 'cheetah-popover-inner-content')
+                    if not dropdown_text:
+                        break
+                    else:
+                        continue
 
             else:
                 print('位置获取失败')
-                time.sleep(10)
+                time.sleep(5)
 
-                # 获取下拉文本列表
-        dropdown = browser.find_element(By.CLASS_NAME, 'cheetah-popover-inner-content')
-        dropdown_text = dropdown.text
         if not dropdown_text:
             break
+        else:
+            continue
