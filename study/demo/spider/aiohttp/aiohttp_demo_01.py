6bd5cd8c18ce84a407598f17d24527c90a7b1b74
admin <240746804@qq.com>
2023-05-17 14:50:19 +0800

    230517-1

--- study/demo/spider/aiohttp/aiohttp_demo_01.py
+++ study/demo/spider/aiohttp/aiohttp_demo_01.py
@@ -2,16 +2,26 @@ import aiohttp
 import asyncio
 import logging
 import json
+from motor.motor_asyncio import AsyncIOMotorClient
 
+# 定义常量
+MONGO_CONNECTION_STRING = 'mongodb://localhost:27017'
+MONGO_DB_NAME = 'books'
+MONGO_COLLECTION_NAME = 'books'
+
+# 获取客户端、数据库、连接器
+client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
+db = client[MONGO_DB_NAME]
+collection = db[MONGO_COLLECTION_NAME]
 
 # 初始化日志配置
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - $(levelname)s: %(message)s')
 
 # 定义常量
-INDEX_URL = 'https://spa5.scrape.center/api/book/limit=18&offset={offset}'
+INDEX_URL = 'https://spa5.scrape.center/api/book/?limit=18&offset={offset}'
 DETAIL_URL = 'https://spa5.scrape.center/api/book/{id}'
 PAGE_SIZE: int = 18
-PAGE_NUMBER: int = 100
+PAGE_NUMBER: int = 5
 CONCURRENCY: int = 5
 
 # 创建一个信号量，控制并发数量
@@ -42,22 +52,47 @@ async def scrape_index(session: aiohttp.ClientSession, page: int) -> dict:
     return await scrape_api(session, url)
 
 
+# 定义保存数据到数据库的方法
+async def save_data(data):
+    logging.info('save data %s', data)
+    if data:
+        # upsert=True 参数表示如果找不到匹配的文档，就插入一条新数据，默认为 False
+        return await collection.update_one({'id': data.get('id')}, {'$set': data}, upsert=True)
+
+
+# 定义爬取详情页的方法
+async def scrape_detail(session, book_id):
+    url = DETAIL_URL.format(id=book_id)
+    data = await scrape_api(session, url=url)
+    await save_data(data)
+
+
 # 定义异步函数main，通过类型标注指定返回值类型为None
 async def main() -> None:
     # 创建一个HTTP请求会话对象
-    session = aiohttp.ClientSession()
+    async with aiohttp.ClientSession() as session:
+        # 创建任务列表，调用scrape_index函数获取指定页码的图书信息
+        scrape_index_tasks = [asyncio.ensure_future(scrape_index(session, page)) for page in range(1, PAGE_NUMBER + 1)]
 
-    # 创建任务列表，调用scrape_index函数获取指定页码的图书信息
-    scrape_index_tasks = [asyncio.ensure_future(scrape_index(session, page)) for page in range(1, PAGE_NUMBER + 1)]
+        # 等待所有异步协程任务完成
+        results = await asyncio.gather(*scrape_index_tasks)
 
-    # 等待所有异步协程任务完成
-    results = await asyncio.gather(*scrape_index_tasks)
+        # 输出获取到的图书信息结果，采用json格式化，并设置ensure_ascii=False保留非ASCII字符，indent设置为2进行缩进
+        logging.info('获取到的图书信息结果: %s,', json.dumps(results, ensure_ascii=False, indent=2))
 
-    # 输出获取到的图书信息结果，采用json格式化，并设置ensure_ascii=False保留非ASCII字符，indent设置为2进行缩进
-    logging.info('获取到的图书信息结果: %s,', json.dumps(results, ensure_ascii=False, indent=2))
+        # 定义一个数组变量保存返回的每个id
+        ids = []
+        for index_data in results:
+            if not index_data:
+                continue
+            for item in index_data.get('results'):
+                ids.append(item.get('id'))
 
-    # 关闭HTTP请求会话对象
-    await session.close()
+        # 爬取详情页
+        scrape_detail_tasks = [asyncio.ensure_future(scrape_detail(session, book_id)) for book_id in ids]
+        await asyncio.wait(scrape_detail_tasks)
+        # 关闭HTTP请求会话对象
+        await session.close()
 
 
 if __name__ == '__main__':
