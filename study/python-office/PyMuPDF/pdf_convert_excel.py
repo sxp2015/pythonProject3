596cb54ff22e8cf7bf8b2ba010d28c6a8d99cf91
admin <240746804@qq.com>
2023-05-26 17:32:13 +0800

    2023.05.26.03

--- study/python-office/PyMuPDF/pdf_convert_excel.py
+++ study/python-office/PyMuPDF/pdf_convert_excel.py
@@ -1,21 +1,22 @@
-import os
-import fitz
-import io
-from PIL import Image
-import tabula
-import pandas as pd
+# 导入必要的库
+import os            # 处理文件和目录路径相关的功能
+import fitz          # 操作 PDF 文件的库 PyMuPDF
+import io            # 读取二进制数据的库
+from PIL import Image    # 处理图片的库
+import tabula        # 读取表格数据的库
+import pandas as pd  # 处理表格数据的库
 
-# PDF文件路径
+# PDF 文件路径
 PDF_FILE_PATH = os.path.join('pdf_files/查验报告.pdf')
 
-# 打开PDF文件
+# 打开 PDF 文件
 pdf_file = PDF_FILE_PATH
 pdf_document = fitz.open(pdf_file)
 
-# 创建一个空的DataFrame，用于存储所有表格数据
+# 创建一个空的 DataFrame，用于存储所有表格数据
 pdf_all = pd.DataFrame()
 
-# 遍历PDF中的每一页
+# 遍历 PDF 中的每一页
 for page_num in range(pdf_document.page_count):
     # 获取当前页
     page = pdf_document[page_num]
@@ -25,10 +26,9 @@ for page_num in range(pdf_document.page_count):
 
     # 遍历当前页中的所有文本块
     for block in page_blocks:
-        print(block)
         # 如果文本块是一张图片
         if block[0] == 1:
-            # 读取图片数据并保存为JPEG文件
+            # 读取图片数据并保存为 JPEG 文件
             img_data = block[4].get_raw_data('png')
             image = Image.open(io.BytesIO(img_data))
             image.save('image{}.jpeg'.format(page_num+1))
@@ -39,11 +39,11 @@ for page_num in range(pdf_document.page_count):
             bbox = block[4]
             x1, y1, x2, y2 = bbox
 
-            # 使用tabula-py库读取表格数据
+            # 使用 tabula-py 库读取表格数据
             df: pd.DataFrame = tabula.read_pdf(pdf_file, pages=str(page_num+1), area=[y1, x1, y2, x2], output_format='dataframe', lattice=True)
 
-            # 将读取的表格数据添加到新的DataFrame中
+            # 将读取的表格数据添加到新的 DataFrame 中
             pdf_all = pd.concat([pdf_all, df], axis=0)
 
-# 将所有读取的表格数据保存到新的CSV文件中
+# 将所有读取的表格数据保存到新的 CSV 文件中
 pdf_all.to_csv('all_tables.csv', index=False)
