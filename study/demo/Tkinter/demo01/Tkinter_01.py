a8b0feff2962d864d74905cec97fe96ae1058793
admin <240746804@qq.com>
2023-07-10 17:31:29 +0800

    2023.07.10-01

--- study/demo/Tkinter/demo01/Tkinter_01.py
+++ study/demo/Tkinter/demo01/Tkinter_01.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import *
+from PIL import Image, ImageTk
 
 # 创建根窗口
 container = tkinter.Tk()
@@ -38,10 +39,31 @@ container.config(bg='#087360')
 # 设置LOGO图标
 container.iconbitmap("logo/logo.ico")
 
+# 创建一个Label标签 bg 背景色  fg 前景色  anchor九宫格位置 ,anchor='nw' (默认上下左右居中，参数
+# nw (North West) 西北方向
+# ne (North East) 东北方向
+# w (West) 正西方向
+# sw (South West) 西南方向
+# center  居中方向
+# se) (South East) 东南方向
+# anchor 的参数设置也可以使用内建大写常数，例如，nw 使用 NW、n使用N  width=30,height=5, anchor=S
+# wraplength = 40 文字长度达到40个像素后换行
+# compound='left' 图像bitmap与文字text的位置关系
+# relief 轮廓边框凹凸效果:flat（无效果）,groove（平面）,raised(凸出来),ridge,solid,sunken（凹进去）
+# padx/pady文字与控件的横纵内边距
+# PhotoImage 可以直接打开PNG,GIF图片
+png_image = PhotoImage(file='./images/north_gate.png')
+# 如果是JPG图像格式，需要借助Pillow库的Image和ImageTk
+jpg_image = Image.open('./images/north_gate.jpg')
+jpg_image_tk = ImageTk.PhotoImage(jpg_image)
+# bitmap 参数和 image 参数不能共存
+label = Label(container, text='郴州建活塑胶科技有限公司', bg='#ffffff', fg='#087360', bitmap='warning', font='Helvetic 14 bold',
+              compound='left', relief='sunken', padx=10, pady=5, image=jpg_image_tk)
 
-# 创建一个Label标签
-label = Label(container, text='郴州建活塑胶科技有限公司', bg='#ffffff', fg='#087360',anchor='nw')
+#  这样写也可以
+#  Label(container, text='郴州建活塑胶科技有限公司', bg='#ffffff', fg='#087360',anchor='nw').pack()
 
+# 包装窗口的Widget 控件和定位窗口的对象
 label.pack()
 
 # 输出版本号
---
+++ study/demo/Tkinter/demo01/Tkinter_02.py
@@ -0,0 +1,42 @@
+# 导入 tkinter 模块
+from tkinter import *
+
+# 定义一个全局计数器变量
+counter = 0
+
+
+# 定义一个函数用于运行计数器
+def run_counter(digit):
+    # 定义内部函数用于执行计数
+    def counting():
+        # 使用 global 关键字声明全局变量
+        global counter
+        # 计数器加1
+        counter += 1
+        # 更新标签的文本内容
+        digit.config(text=str(counter))
+        # 每隔一秒调用 counting 函数
+        digit.after(1000, counting)
+
+    # 调用 counting 函数开始计数
+    counting()
+
+
+# 创建根窗口
+container = Tk()
+
+# 设置窗口标题
+container.title('内容自动加1')
+
+# 创建标签，设置背景、前景颜色、高度、宽度和字体样式,
+# cursor='heart' 鼠标经过悬浮时，显示心形鼠标
+digit = Label(container, bg='yellow', fg='blue', height=3, width=10, font='Helvetica 20 bold', cursor='heart')
+
+# 将标签添加到窗口中的合适位置
+digit.pack()
+
+# 运行计数器函数，传入标签作为参数
+run_counter(digit)
+
+# 进入 tkinter 的主循环，等待事件的发生
+container.mainloop()
